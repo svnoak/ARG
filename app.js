4b47e25c4bd3a7f77b474b3ff288df06823a2aca
Alex <zetterstrom.alexander@gmail.com>
2022-04-12 12:54:58 +0200

    change notif

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js').then((reg) => console.log('service worker registered', reg)).catch((error) => console.log('service worker not registered', error));
+    navigator.serviceWorker.register('sw.js').then(showNotification(), (reg) => console.log('service worker registered', reg)).catch((error) => console.log('service worker not registered', error));
 }
 
 
