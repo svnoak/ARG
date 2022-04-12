7936ded92716699a9f07c74c81669bd8651126b1
Alex <zetterstrom.alexander@gmail.com>
2022-04-12 13:13:01 +0200

    appjs test 2

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-/*
+
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js').then((reg) => console.log('service worker registered', reg)).catch((error) => console.log('service worker not registered', error));
+    navigator.serviceWorker.register('sw.js').then((reg) => reg.showNotification('registered!')).catch((error) => console.log('service worker not registered', error));
 }
-*/
+
 
 
 Notification.requestPermission(status => {
     console.log('notification permission status:' , status)
 });
 
-
-navigator.serviceWorker.getRegistration('sw.js').then(function(registration) {
-    if(registration){
-     console.log("sw registered");
+/*
+function displayNotification() {
+    if (Notification.permission === 'granted') {
+        navigator.serviceWorker.getRegistration('sw.js').then(reg => {
+            reg.showNotification('Hello world!');
+        })
     }
-  });
-
+}
+*/
