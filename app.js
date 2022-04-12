9b61d8da98957eb44440c1f3cf31558bd19f0dbf
Alex <zetterstrom.alexander@gmail.com>
2022-04-12 13:07:27 +0200

    appjs test

--- app.js
+++ app.js
@@ -9,12 +9,9 @@ Notification.requestPermission(status => {
     console.log('notification permission status:' , status)
 });
 
-function displayNotification() {
-    if (Notification.permission === 'granted') {
+
         navigator.serviceWorker.getRegistration('sw.js').then(reg => {
             console.log(reg);
         })
-    }
-}
+    
 
-displayNotification();
