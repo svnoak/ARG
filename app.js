9c337aedf56d19fda98d86761be218b8a3cdea4a
Alex <zetterstrom.alexander@gmail.com>
2022-04-12 12:23:43 +0200

    updated app.js

--- app.js
+++ app.js
@@ -2,15 +2,6 @@ if('serviceWorker' in navigator){
     navigator.serviceWorker.register('sw.js').then((reg) => console.log('service worker registered', reg)).catch((error) => console.log('service worker not registered', error));
 }
 
-if('serviceWorker' in navigator){
-navigator.serviceWorker.getRegistration().then(reg => {
-    reg.pushManager.subscribe({
-        userVisibleOnly: true
-    }).then(sub => {
-        // send sub.toJSON() to server
-    });
-});
-}
 
 Notification.requestPermission(status => {
     console.log('notification permission status:' , status)
