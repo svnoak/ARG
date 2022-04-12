ffa167e89179d991e4b54ac921ee56918fefd151
Alex <zetterstrom.alexander@gmail.com>
2022-04-12 12:22:28 +0200

    updated values app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js').then((reg) => console.log('service worker registered', reg)).catch((error) => console.log('service worker not registered', error));
+    navigator.serviceWorker.register('sw.js').then((reg) => console.log('service worker registered', reg)).catch((error) => console.log('service worker not registered', error));
 }
 
 if('serviceWorker' in navigator){
@@ -17,7 +17,7 @@ Notification.requestPermission(status => {
 });
 
 function displayNotification() {
-    if (Notification.permission === 'Granted') {
+    if (Notification.permission === 'granted') {
         navigator.serviceWorker.getRegistration().then(reg => {
             reg.showNotification('Hello world!');
         })
