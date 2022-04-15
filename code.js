43a2b19e226bc4c08ff658965f0a18bc58d12570
svnoak <kimpfimp@posteo.net>
2022-04-15 16:58:34 +0200

    adding tips

--- code.js
+++ code.js
@@ -4,5 +4,9 @@ function checkPass(){
     const input = document.querySelector("input").value;
     let pass = input.trim().toLowerCase();
     const correct = "barnen är i fara";
-    pass == correct ? alert("Rätt svar!") : alert("Du förstår inte riktigt än.");
+    if( pass == 07061105 || pass == 76115 ){
+        alert("Siffrorna är vägen, inte svaret.");
+    }else{
+        pass == correct ? alert("Rätt svar!") : alert("Du förstår inte riktigt än.");
+    }
 }
--- de/code.js
+++ de/code.js
@@ -4,5 +4,9 @@ function checkPass(){
     const input = document.querySelector("input").value;
     let pass = input.trim().toLowerCase();
     const correct = "kinder sind in gefahr";
-    pass == correct ? alert("Richtig!") : alert("Du verstehst noch nicht.");
+    if( pass == 07061105 || pass == 76115 ){
+        alert("Die Zahlen sind der Weg, nicht die Antwort.");
+    }else{
+        pass == correct ? alert("Richtig!") : alert("Du verstehst noch nicht.");
+    }
 }
