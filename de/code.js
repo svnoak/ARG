f1aabc6a39a51ab720f1557dbe454bd480adf232
svnoak <kimpfimp@posteo.net>
2022-04-15 17:00:17 +0200

    fixing german tip

--- de/code.js
+++ de/code.js
@@ -4,7 +4,7 @@ function checkPass(){
     const input = document.querySelector("input").value;
     let pass = input.trim().toLowerCase();
     const correct = "kinder sind in gefahr";
-    if( pass == 07061105 || pass == 76115 ){
+    if( pass == 07020104 || pass == 7214 ){
         alert("Die Zahlen sind der Weg, nicht die Antwort.");
     }else{
         pass == correct ? alert("Richtig!") : alert("Du verstehst noch nicht.");
