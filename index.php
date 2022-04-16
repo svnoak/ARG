d77f303aff80fd710a50a3c9d182878085133989
svnoak <kimpfimp@posteo.net>
2022-04-16 23:11:58 +0200

    u

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 require_once("config.php");
 
+/* $result = mysqli_query($mysqli, "SHOW COLUMNS FROM email");
+
+if($result){
+  // Cycle through results
+ while ($row = $result->fetch_object()){
+     $user_arr[] = $row;
+ }
+ var_dump($user_arr);
+}
+
+
+
+$name = 'Kim';
+$password = 1234;
+
+$stmt = $mysqli->prepare("INSERT INTO user (name, password) VALUES(?, ?)");
+$stmt->bind_param("si", $name, $password);
+$stmt->execute(); */
+
 ?>
