f319343b0d9004ee7a37d1566ed746ebabc8810e
svnoak <kimpfimp@posteo.net>
2022-04-16 22:49:09 +0200

    adding php mysql config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// Basic connection settings
+$databaseHost = 'localhost';
+$databaseUsername = 'varg';
+$databasePassword = 'q3FMeGyWwfMnHzt4BKW9HEKfTeUAWeFMFcUacxs3SMFqkZwZE4kLHeUvfeAKbZxD';
+$databaseName = 'varg';
+
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
