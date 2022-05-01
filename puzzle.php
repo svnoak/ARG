91198988518cbaf9fedc676f5f179e01fbc4a617
svnoak <kimpfimp@posteo.net>
2022-05-01 12:25:52 +0200

    fixing query

--- puzzle.php
+++ puzzle.php
@@ -6,13 +6,13 @@ class Puzzle
 
     static function getByOrder($order){
         global $mysqli;
-        $dialog = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE order = $order");
+        $dialog = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE `order` = $order");
         return $dialog->fetch_object();
     }
 
     static function getPuzzlesByNPCId($id){
         global $mysqli;
-        $puzzle = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE type = 'puzzle' AND npc = $id");
+        $puzzle = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE `type` = 'puzzle' AND `npc` = $id");
             while ($row = $puzzle->fetch_object()){
                 $puzzle_arr[] = $row;
             }
@@ -21,7 +21,7 @@ class Puzzle
 
     static function checkAnswerByPuzzleOrder($order, $answer){
         global $mysqli;
-        $query = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE order = '$order' AND type = 'puzzle'");
+        $query = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE `order` = $order AND `type` = 'puzzle'");
         return $query;
         /* $puzzle = $query->fetch_object();
         $path = "./$puzzle.type/$puzzle.jsonLink";
