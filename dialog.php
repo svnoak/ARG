e7ebc77e4f801166ded572ad15da327e52527c83
svnoak <kimpfimp@posteo.net>
2022-05-01 18:37:42 +0200

    remove obsolete code

--- dialog.php
+++ dialog.php
@@ -52,30 +52,6 @@ class Dialog
         }
         return $dialogRespone && $rewardResponse && $archivePlaceResponse;
     }
-/* 
-
-
-    static function getDialogsByNpcId($id){
-        global $mysqli;
-        $dialog = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE npc = $id AND type = 'dialog'");
-        while ($row = $dialog->fetch_object()){
-            $dialog_arr[] = $row;
-        }
-       return $dialog_arr;
-    }
-
-    static function getByOrder($order){
-        global $mysqli;
-        $dialog = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE order = $order");
-        return $dialog->fetch_object();
-    }
-
-    
-
-    static function next($userID){
-        return getLastDialog($userID) + 1;
-    } */
-
     
 }
 
