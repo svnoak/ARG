a49f0920cf1f48a99c5b0a17d020317bca7bff59
svnoak <kimpfimp@posteo.net>
2022-05-01 15:34:01 +0200

    testing

--- place.php
+++ place.php
@@ -30,12 +30,11 @@ class Place
     static function getDialog($placeID, $userID){
         global $mysqli;
         $lastDialog = Dialog::getLastDialog($userID);
-        $dialog_query = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE place = $placeID AND `order` > $lastDialog");
-        return $dialog_query;
-        /* while ($row = $dialog_query->fetch_object()){
+        $dialogs = mysqli_query($mysqli, "SELECT * FROM Dialog WHERE place = $placeID AND `order` > $lastDialog");
+        while ($row = $dialogs->fetch_object()){
             $dialog_arr[] = $row;
         }
-        return $dialog_arr; */
+       return $dialog_arr;
     }
 
     static function getData($placeID, $userID){
