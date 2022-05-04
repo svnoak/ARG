b34f419cf3dcdb067f4811e327e42cc5461afa0f
svnoak <kimpfimp@posteo.net>
2022-05-04 16:23:27 +0200

    loading in correct data and creating the correct npc image

--- src/camera.js
+++ src/camera.js
@@ -5,10 +5,12 @@ import React from 'react';
 class Camera extends React.Component {
   constructor(props){
     super(props)
+    localStorage.setItem("arg_userID", 1);
     this.state = {
       locations: JSON.parse(localStorage.getItem("locations")),
       currentPlace: "",
-      userID: localStorage.getItem("arg_userID")
+      userID: localStorage.getItem("arg_userID"),
+      playerAtLocation: false
   } 
   }
 
@@ -37,47 +39,58 @@ class Camera extends React.Component {
     this.setState({locations: JSON.parse(localStorage.getItem("locations"))});
   }
 
-  success(pos){
+  async success(pos){
     let crd = pos.coords;
-    this.PlayerIsNearLocation(crd);
+    const playerIsNearLocation  = await this.PlayerIsNearLocation(crd);
+    this.setState({playerAtLocation: playerIsNearLocation});
+    console.log(this.state.playerAtLocation);
   }
 
   async PlayerIsNearLocation(coords){
-    const playerLocation = { latitude: "55.606619", longitude: "12.997648" };
+    const playerLocation = { latitude: "55.599945", longitude: "13.005303" };
     for( let location of this.state.locations ){
       const locationCoords = { latitude: location.latitude, longitude: location.longitude };
       const distance = haversine(playerLocation, locationCoords);
       if( distance < location.area ){
-        /* console.log(`Player is ${Math.round(distance)} meters from ${location.name}`); */
-        const placeData = await this.getPlaceData(location.id, this.state.userID);
-        /* console.log(placeData); */
-        
+        await this.setPlaceState(location.id, this.state.userID);
+        return true;
       }
-    }
+    } 
+    return false
   }
 
   error(err){
     console.warn('ERROR(' + err.code + '): ' + err.message);
   }
 
-  async getPlaceData(placeID, userID){
+  async setPlaceState(placeID, userID){
     const request = new Request(`https://dev.svnoak.net/api/place/${placeID}/${userID}`);
     const response = await fetch(request);
     const json = await response.json();
-    console.log(json);
     this.setState({currentPlace: json});
   }
 
   render(){
+    const isAtLocation = this.state.playerAtLocation;
+    let ar;
+    const data = this.state.currentPlace;
+    if( isAtLocation ){
+      ar = <AR
+            place = {data.place}
+            npc = {data.npc}
+            dialog = {data.dialog}
+          />
+    } else{
+      ar = "";
+    }
     return(
       <div id="cameraScene">
         <Waiting />
-        {/* <AR /> */}
+        { ar }
         <DialogBox />
       </div>
     )
   }
-
 }
 
 function Waiting(){
@@ -97,27 +110,30 @@ function DialogBox(){
   )
 }
 
-function AR(){
-  const AFRAME = window.AFRAME;
+class AR extends React.Component {
+  constructor(props){
+    super(props)
+  }
 
-  return (
-    
-         <a-scene  vr-mode-ui="enabled: false" arjs="sourceType: webcam; debugUIEnabled: false;" inspector="" keyboard-shortcuts="" screenshot="" device-orientation-permission-ui="" aframe-inspector-removed-embedded="undefined" cursor="rayOrigin: mouse">
-         <a-assets>
-          <img id="1" src="https://dev.svnoak.net/assets/images/trollunge.png"></img>
-          <img id="0" src="https://dev.svnoak.net/assets/images/trollmor.png"></img>
-        </a-assets>
-          <a-image clickhandler id="npc" src="#0" npc look-at="[camera]" position="0 0 -6" height="2" width="5"></a-image>
-          <a-image clickhandler id="npc" src="#1" npc look-at="[camera]" position="0 0 -6" height="2" width="5"></a-image>
+  render() {
+    const AFRAME = window.AFRAME;
+    return (
+      <a-scene  vr-mode-ui="enabled: false" arjs="sourceType: webcam; debugUIEnabled: false;" inspector="" keyboard-shortcuts="" screenshot="" device-orientation-permission-ui="" aframe-inspector-removed-embedded="undefined" cursor="rayOrigin: mouse">
+      <a-assets>
+       <img id="image" src={"https://dev.svnoak.net/assets/images/" + this.props.npc.imageLink}></img>
+     </a-assets>
+       <a-image clickhandler id="npc" src="#image" npc look-at="[camera]" position="0 0 -6" height="2" width="1"></a-image>
 
-          <a-camera camera look-controls rotation-reader arjs-look-controls='smoothingFactor: 0.1'>
-              {/* <a-entity geometry="primitive: ring; radiusInner: .5; radiusOuter: 8;" material="color: black; shader: flat;" position="0 0 -1"></a-entity> */}
-              <a-entity cursor="fuse: true;" position="0 0 -1" geometry="primitive: circle; radius: .01" material="color: black; shader: flat; opacity: 0.2" raycaster=""></a-entity>
-          </a-camera>  
-              
-    <div className="a-loader-title" style={{display: 'none'}}></div>
-    </a-scene>
-     );
+       <a-camera camera look-controls rotation-reader arjs-look-controls='smoothingFactor: 0.1'>
+           {/* <a-entity geometry="primitive: ring; radiusInner: .5; radiusOuter: 8;" material="color: black; shader: flat;" position="0 0 -1"></a-entity> */}
+           <a-entity cursor="fuse: true;" position="0 0 -1" geometry="primitive: circle; radius: .01" material="color: black; shader: flat; opacity: 0.2" raycaster=""></a-entity>
+       </a-camera>  
+           
+ <div className="a-loader-title" style={{display: 'none'}}></div>
+ </a-scene>
+  );
+  }
+  
 }
  
 export default Camera;
