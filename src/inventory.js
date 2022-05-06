5f9d37d999c78ea1fc5333d3180c3472e7074e25
Alex <zetterstrom.alexander@gmail.com>
2022-05-06 02:39:35 +0200

    login added

--- src/inventory.js
+++ src/inventory.js
@@ -1,7 +1,23 @@
+import { createElement } from "react";
+
 const Inventory = () => {
-    return ( 
-        <h1 className="tjenare">inventory</h1>
-     );
-}
- 
+
+
+
+
+    
+createElement("div")
+
+
+  return (
+    <div className="wrapper">
+      <h1 className="tjenare">inventory</h1>
+
+      <div className="grid">
+
+      </div>
+    </div>
+  );
+};
+
 export default Inventory;
