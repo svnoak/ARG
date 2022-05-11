21ccfb38dcaf2c98b463ed524bb3c701150dcd22
svnoak <kimpfimp@posteo.net>
2022-05-11 23:16:28 +0200

    update

--- dialog.php
+++ dialog.php
@@ -33,7 +33,7 @@ class Dialog
             }
         }
         $dialogResponse = mysqli_query($mysqli, "UPDATE User SET lastDialog=$dialogID WHERE id = $userID");
-        $endingResponse = mysqli_query($mysqli, "UPDATE User SET ending='$ending' WHERE id = $userID");
+        $endingResponse = mysqli_query($mysqli, "UPDATE User SET ending=null WHERE id = $userID");
         return $endingResponse;
         $rewardQuery = mysqli_query($mysqli, "SELECT reward FROM Dialog WHERE id = $dialogID");
         $rewardID = $rewardQuery->fetch_object()->reward;
