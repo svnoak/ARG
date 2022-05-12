f59f37fc7e45535696e6e10d31af1ecf2ebd96d3
svnoak <kimpfimp@posteo.net>
2022-05-12 15:50:52 +0200

    u

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Chat from "./chat.js";
-import Archive from "./archive.js";
-import Camera from "./camera.js";
-import NavBar from "./navBar.js";
-import Login from "./login.js";
-import Locations from "./locations.js";
-import Creatures from "./creatures.js";
-import Inventory from "./inventory.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/locations" element={<Locations />} />
-      <Route path="/archive" element={<Archive />} />
-      <Route path="/camera" element={<Camera />} />
-      <Route path="/creatures" element={<Creatures />} />
-      <Route path="/inventory" element={<Inventory />} />
-    </Routes>
-    <NavBar />
-  </BrowserRouter>
-);
+  <App />
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
