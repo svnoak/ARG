00b6a139c2f1307594ae08e2911972c939089a57
svnoak <kimpfimp@posteo.net>
2022-05-12 16:12:05 +0200

    Revert "Revert "inventory, creatures, locations""
    This reverts commit 0084258c085ff462f0c120e1ae356a59b9866648.

--- src/locations.js
+++ src/locations.js
@@ -1,18 +1,17 @@
-import { createElement } from "react";
-
 import React, { useState } from "react";
 
-const Inventory = () => {
+const Locations = () => {
+  const [viewer, setViewer] = useState([]);
   const [data, setData] = useState([]);
   localStorage.setItem("id", "18");
   let loggedinUser = localStorage.getItem("id");
 
-  if (data.length == 0) {
+  if (data.length === 0) {
     getSomeData();
   }
   function getSomeData() {
     const request = new Request(
-      "https://dev.svnoak.net/api/inventory/item/" + loggedinUser
+      "https://dev.svnoak.net/api/inventory/place/" + loggedinUser
     );
     fetch(request)
       .then((response) => response.json())
@@ -21,33 +20,34 @@ const Inventory = () => {
       });
   }
 
+  function buttonClick(propid) {
+    setViewer(data[propid - 1].imageLink);
+  }
+
   return (
     <div className="wrapper">
       <div className="buttongrid">
         {data.map(function (prop) {
           return (
-            <div key={prop.id} className="buttons">
-              <p>{prop.name}</p>
-              <img
-                src={"https://dev.svnoak.net/assets/images/" + prop.imageLink}
-              />
+            <div
+              key={prop.id}
+              onClick={() => {
+                buttonClick(prop.id);
+              }}
+              className="buttons"
+            >
+           
             </div>
           );
         })}
       </div>
       <div className="viewpanel">
-        {data.map(function (prop) {
-          return (
-            <div key={prop.id} className="images">
-              <img
-                src={"https://dev.svnoak.net/assets/images/" + prop.imageLink}
+      <img
+                alt ={viewer} src={"https://dev.svnoak.net/assets/images/" + viewer}
               />
-            </div>
-          );
-        })}
       </div>
     </div>
   );
 };
 
-export default Inventory;
+export default Locations;
