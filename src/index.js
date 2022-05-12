23f43d9445e3ab57fe5497c850401e8dca56ddb2
svnoak <kimpfimp@posteo.net>
2022-05-12 15:52:19 +0200

    manual redo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from "./App";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+  } from "react-router-dom";
+import Chat from "./chat.js";
+import Archive from "./archive.js";
+import Camera from "./camera.js";
+import NavBar from "./navBar.js";
+import Login from "./login.js";
+import Locations from "./locations.js";
+import Creatures from "./creatures.js";
+import Inventory from "./inventory.js";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <App />
+  <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Chat />} />
+      <Route path="/locations" element={<Locations />} />
+      <Route path="/archive" element={<Archive />} />
+      <Route path="/camera" element={<Camera />} />
+      <Route path="/creatures" element={<Creatures />} />
+      <Route path="/inventory" element={<Inventory />} />
+  </Routes>
+  <NavBar />
+  </BrowserRouter>  
   );
 
 // If you want to start measuring performance in your app, pass a function
